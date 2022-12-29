f79609c6cbb6749b40e2ac1d2ff1110604b9c9a2
Miha Krajnc <miha.krajnc@dewesoft.com>
2022-12-29 15:35:53 +0100

    Add consul

---
+++ api/src/main/java/api/v1/filter/MaintenanceFilter.java
@@ -0,0 +1,28 @@
+package api.v1.filter;
+
+import services.config.RestProperties;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+@ApplicationScoped
+public class MaintenanceFilter implements ContainerRequestFilter {
+
+    @Inject
+    private RestProperties restProperties;
+
+    @Override
+    public void filter(ContainerRequestContext ctx) throws IOException {
+        if (restProperties.getMaintenanceMode()) {
+            ctx.abortWith(Response.status(Response.Status.FORBIDDEN)
+                    .entity("{\"message\" : \"Maintenance mode enabled\"}")
+                    .build());
+        }
+    }
+}
